a5720afff90578acfa5837549e25cf07a445623b
Samuel Ribeiro de Freitas <samuelr@take.net>
2020-01-09 17:07:39 -0300

    Added cpf validation service

--- controllers/cpfController.js
+++ controllers/cpfController.js
@@ -1,3 +1,21 @@
+const cpfUtils = require('cpf');
+
 module.exports.validateCpf = (req,res,next) => {
-    res.send('Cpf validate');
+    try{
+
+        var cpf = cpfUtils.format(req.query.cpf);
+
+        if(cpfUtils.isValid(cpf)){
+
+            res.status(200).send(cpf);
+
+        }
+
+
+    }
+    catch(err){
+
+        res.status(200).send('Formato inválido');
+
+    }
 }
